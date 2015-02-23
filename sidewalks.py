e15ee0eb53c832936f231ab08dc159043ef8cc19
Sean Wright <sean@mmi.technology>
2015-02-23 16:06:43 -0700

    sidewalk.py obtains a snow report from noaa.  added beautiful soup for parsing. returning soup text.

--- sidewalks.py
+++ sidewalks.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import requests
+from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def returnSnow():
+	snowReportResponse = requests.get('http://www.nws.noaa.gov/data/obhistory/KBDU.html')
+	forecastResponse = requests.get('https://api.forecast.io/forecast/a1dcd3de7338008cc494d5677bcee08e/40.04,105.23')
+
+	snowReportSoup = BeautifulSoup(snowReportResponse.text)
+
+	return snowReportSoup.text
 
 if __name__ == "__main__":
     app.run()
