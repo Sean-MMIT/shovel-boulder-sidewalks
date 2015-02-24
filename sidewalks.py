2a1ed561909499e1939ed6ca72c940b5990c18f0
Sean Wright <sean@mmi.technology>
2015-02-23 20:35:09 -0700

    Normalized tabs to four spaces and added ordinal notation to date.

--- sidewalks.py
+++ sidewalks.py
@@ -7,19 +7,23 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
-def index():	
-	snowData = returnLastSnowDateAndTime()
-	return render_template('index.html', snowData=snowData)
+def index():
+    snowData = returnLastSnowDateAndTime()
+    return render_template('index.html', snowData=snowData)
 
 def returnLastSnowDateAndTime():
-	snowReportResponse = requests.get('http://www.nws.noaa.gov/data/obhistory/KBDU.html')
-	forecastResponse = requests.get('https://api.forecast.io/forecast/a1dcd3de7338008cc494d5677bcee08e/40.04,105.23')
-	snowReportSoup = BeautifulSoup(snowReportResponse.text)
-	lastSnowReport = snowReportSoup.find(text=re.compile(r'Snow'))
-	snowRow = lastSnowReport.parent.parent
-	snowDate = snowRow.find("td").string
-	snowTime = snowRow.find(text=re.compile(r'..:..')).string
-	return { 'lastSnowReport':lastSnowReport, 'snowDate':snowDate, 'snowTime':snowTime }
+    snowReportResponse = requests.get('http://www.nws.noaa.gov/data/obhistory/KBDU.html')
+    forecastResponse = requests.get('https://api.forecast.io/forecast/a1dcd3de7338008cc494d5677bcee08e/40.04,105.23')
+    snowReportSoup = BeautifulSoup(snowReportResponse.text)
+    lastSnowReport = snowReportSoup.find(text=re.compile(r'Snow'))
+    snowRow = lastSnowReport.parent.parent
+    snowDate = snowRow.find("td").string
+    snowTime = snowRow.find(text=re.compile(r'..:..')).string
+    return { 'lastSnowReport':lastSnowReport, 'snowDate':ord(snowDate), 'snowTime':snowTime }
+
+def ord(n):
+    n = int(n)
+    return str(n)+("th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th"))
 
 if __name__ == "__main__":
     app.run()
