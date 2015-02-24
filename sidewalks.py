d6d9cf3be04ba4fb58ec1e54d695c79d3cbffa2d
Sean Wright <sean@mmi.technology>
2015-02-23 20:22:31 -0700

    Configured project to load html template.

--- sidewalks.py
+++ sidewalks.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import render_template
 import requests
 from bs4 import BeautifulSoup
 import re
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
+def index():	
+	snowData = returnLastSnowDateAndTime()
+	return render_template('index.html', snowData=snowData)
+
 def returnLastSnowDateAndTime():
 	snowReportResponse = requests.get('http://www.nws.noaa.gov/data/obhistory/KBDU.html')
 	forecastResponse = requests.get('https://api.forecast.io/forecast/a1dcd3de7338008cc494d5677bcee08e/40.04,105.23')
@@ -13,7 +19,7 @@ def returnLastSnowDateAndTime():
 	snowRow = lastSnowReport.parent.parent
 	snowDate = snowRow.find("td").string
 	snowTime = snowRow.find(text=re.compile(r'..:..')).string
-	return lastSnowReport + " was reported on " + snowDate + " at " + snowTime
+	return { 'lastSnowReport':lastSnowReport, 'snowDate':snowDate, 'snowTime':snowTime }
 
 if __name__ == "__main__":
     app.run()
