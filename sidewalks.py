5adaf3eea82acffb63c1b47825f01f63d86f7dcf
Sean Wright <sean@mmi.technology>
2015-02-23 18:51:42 -0700

    Renamed returnSnow function and collapsed.

--- sidewalks.py
+++ sidewalks.py
@@ -5,18 +5,14 @@ import re
 app = Flask(__name__)
 
 @app.route("/")
-def returnSnow():
+def returnLastSnowDateAndTime():
 	snowReportResponse = requests.get('http://www.nws.noaa.gov/data/obhistory/KBDU.html')
 	forecastResponse = requests.get('https://api.forecast.io/forecast/a1dcd3de7338008cc494d5677bcee08e/40.04,105.23')
-
 	snowReportSoup = BeautifulSoup(snowReportResponse.text)
-
 	lastSnowReport = snowReportSoup.find(text=re.compile(r'Snow'))
-
 	snowRow = lastSnowReport.parent.parent
 	snowDate = snowRow.find("td").string
 	snowTime = snowRow.find(text=re.compile(r'..:..')).string
-
 	return lastSnowReport + " was reported on " + snowDate + " at " + snowTime
 
 if __name__ == "__main__":
